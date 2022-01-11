190f6bd4e36f73eab602007c6315b812c50f2d06
Perry <59033672+perryliuofficial@users.noreply.github.com>
2022-01-11 14:24:20 +0000

    added comments to solution1.py

--- solution1.py
+++ solution1.py
@@ -1,10 +1,10 @@
 def find_outlier(integers):
-    even = [i for i in integers if i % 2 == 0]
-    odd = [i for i in integers if i % 2 != 0]
+    even = [i for i in integers if i % 2 == 0] # add even numbers to list
+    odd = [i for i in integers if i % 2 != 0] # add odd numbers to list
     
-    if len(even) > len(odd):
+    if len(even) > len(odd): # if there are more even than odd numbers then return odd
         return odd[0]
-    else:
+    else: # if there are more odd than even numbers then return even
         return even[0]
 
 print(find_outlier([2, 4, 6, 8, 10, 3]))
